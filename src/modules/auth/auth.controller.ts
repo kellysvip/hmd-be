cde8b221a8e5c4e5428d16e1a5ac8a58813fa073
Kelly <kelly@MacBook-Pro.local>
2025-02-20 11:16:51 +0700

    feat(login-tdd-green-bar): create dto to validate login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Headers, HttpCode, HttpStatus, Post, Req, UsePipes } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { SwaggerTags } from '../../common/constants/enums/swagger-tags.enum';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
+import validationPipe from '../../validation-pipe';
 
 @Controller('auth')
 @ApiTags(SwaggerTags.AUTH)
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) { }
+
+    @Post('login')
+    @HttpCode(HttpStatus.OK)
+    @UsePipes(validationPipe())
+    async login(@Headers('deviceId') deviceId: Request, @Body() dto: AuthDto) {
+
+        return this.authService.login(Number(deviceId), dto);
+    }
+}
