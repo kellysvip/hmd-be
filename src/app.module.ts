2f98870b0720ed86896333fef7ed0f15107d8600
Kelly <kelly@MacBook-Pro.local>
2025-02-20 17:57:35 +0700

    feat(login-tdd-green-bar): install redis, create middleware to handle burst check

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
 import { AuthModule } from './modules/auth/auth.module';
+import { TokenBucketMiddleware } from './middleware/token-bucket.middleware';
 
 @Module({
   imports: [AuthModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(TokenBucketMiddleware).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,38 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
+import { UnprocessableEntityException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: false,
+      whitelist: true,
+      transform: true,
+      forbidUnknownValues: true,
+      forbidNonWhitelisted: true,
+      skipMissingProperties: false,
+      skipNullProperties: false,
+      skipUndefinedProperties: false,
+      validationError: {
+        target: false,
+        value: true,
+      },
+      exceptionFactory: (errors) => {
+        const formattedErrors = errors.map((err) => {
+          const constraints = err.constraints ?? {};
+          if (constraints.isNotEmpty) {
+            return constraints.isNotEmpty;
+          }
+          return Object.values(constraints)[0] || 'Validation error';
+        });
+
+        return new UnprocessableEntityException(formattedErrors);
+      },
+    }),
+  );
 
   const docConfig = new DocumentBuilder()
     .setTitle('HMD API Documentation')
